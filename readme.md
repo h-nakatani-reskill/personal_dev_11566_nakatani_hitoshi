7ad8fca3df3aaf1d896359861a67c30f59ae6537
student <student@FJ-1025>
2026-05-19 15:39:06 +0900

    feat:カテゴリー調整部分とタスク管理を調整

---
+++ readme.md
@@ -0,0 +1,28 @@
+
+
+## ファイル構成
+
+### Controller
+TaskController
+UserContoroller
+CategoryController
+
+### HTML
+- ログイン関係
+login.html
+addUser.html
+
+- タスク関係
+tasks.html
+addtask.html
+editTask.html
+
+- カテゴリー関係
+categories.html
+addCategory.html
+editCategory/hrml
+
+CREATE DATABASE personal_dev_11566_nakatani_hitoshi OWNER student ENCODING 'UTF-8'; /* DB名 */
+
+
+
--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,5 +1,38 @@
 package com.example.demo.controller;
 
-public class CategoryController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
+public class CategoryController {
+	// カテゴリー一覧表示
+	@GetMapping("/category")
+	public String showCategory() {
+		return "category";
+	}
+
+	// 追加画面表示
+	@GetMapping("/category/add")
+	public String create() {
+		return "categoryAdd";
+	}
+
+	// 追加処理
+	@PostMapping("/category/add")
+	public String add() {
+		return "redirect:/category";
+	}
+
+	// 変更画面表示
+	@GetMapping("/category/edit")
+	public String edit() {
+		return "categoryEdit";
+	}
+
+	// 変更処理
+	@PostMapping("/category/edit")
+	public String update() {
+		return "redirect:/category";
+	}
 }
