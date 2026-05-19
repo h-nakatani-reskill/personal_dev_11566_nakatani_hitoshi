9681b658ac2a6a0c5ec66eca427f302202d42d63
student <student@FJ-1025>
2026-05-19 09:42:12 +0900

    初期設定

---
+++ src/test/java/com/example/demo/PersonalDev11566NakataniHitoshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDev11566NakataniHitoshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
